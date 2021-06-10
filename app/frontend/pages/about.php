1f9d8b4a9c8ffb2e13e3e72ed9ade6fe40ba104c
@hokhang <hokhang99ers@gmail.com>
2021-06-10 21:57:29 +0700

    first commit

---
+++ app/frontend/pages/about.php
@@ -0,0 +1,108 @@
+<div class="hero-wrap hero-bread" style="background-image: url('images/bg_6.jpg');">
+    <div class="container">
+    <div class="row no-gutters slider-text align-items-center justify-content-center">
+        <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-0 bread">Giới thiệu</h1>
+        </div>
+    </div>
+    </div>
+</div>
+<div class="ftco-section ftco-choose ftco-no-pb ftco-no-pt mt-4">
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-md-8 d-flex align-items-stretch">
+                <div class="img" style="background-image: url(https://i.dailymail.co.uk/1s/2019/10/15/00/19716530-0-image-a-5_1571096727350.jpg);"></div>
+            </div>
+            <div class="col-md-4 py-md-5 ftco-animate">
+                <div class="text py-3 py-md-5">
+                    <h1 class="mb-4 mt-3" >Kỹ thuật viên chuyên nghiệp</h1>
+                    <p>Nhiều năm đồng hành cùng phụ nữ trong lĩnh vực chăm sóc và làm đẹp, LINA SPA & BEAUTY SALON tự tin với đội ngũ nhân viên spa chuyên nghiệp được đào tạo tay nghề cao, có kinh nghiệm làm việc trong ngành thẩm mỹ, làm đẹp phù hợp với nhu cầu mong muốn mà bạn yêu cầu.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col-md-5 order-md-last d-flex align-items-stretch">
+                <div class="img" style="background-image: url(images/maymoc.jpg); box-shadow: -10px -10px 5px #c7c5c5; margin-top: -70px"></div>
+            </div>
+            <div class="col-md-7 py-3 py-md-5 ftco-animate">
+                <div class="text text-2 py-md-5">
+            <h2 class="mb-4">Máy móc tiên tiến hiện đại</h2>
+            <p>Hệ thống phòng mổ, phòng hậu phẫu và phòng khám của Spa được thiết kế sang trọng, hiện đại đạt tiêu chuẩn an toàn của Bộ Y tế. Đặc biệt, máy móc, thiết bị của spa cũng được chúng tôi trang bị với những sản phẩm nhập khẩu của những thương hiệu uy tín tại các nước có nền thẩm mỹ phát triển như Hoa Kỳ và Hàn Quốc.</p>
+            <p><a href="service.php" class="btn btn-white px-4 py-3">Xem ngay</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<section class="ftco-section testimony-section">
+    <div class="container">
+        <div class="row justify-content-center mb-5 pb-3">
+        <div class="col-md-7 heading-section ftco-animate text-center">
+            <h2 class="mb-4">Sự hài lòng của khách hàng là niềm vinh hạnh của chúng tôi<br/>LINA Spa & Beauty salon</h2>
+            <p>Với phương châm “Sự hài lòng của khách hàng là thành công của Spa”, Lina Spa tuy mới thành lập nhưng nhận được không ít những phản hồi tích cực và sớm có khách hàng trung thành.</p>
+        </div>
+        </div>
+        <div class="row ftco-animate">
+            <div class="item col-6 col-md-3">
+                <div class="testimony-wrap p-4 pb-5">
+                <div class="user-img mb-5" style="background-image: url(images/xi.jpg)">
+                    <span class="quote d-flex align-items-center justify-content-center">
+                    <i class="icon-quote-left"></i>
+                    </span>
+                </div>
+                <div class="text">
+                    <p class="mb-5 pl-4 line" style="min-height: 135px;">"Điều mà mình ấn tượng nhất là các chuyên viên thẩm mỹ của Sài Gòn Spa rất thân thiện và rất chu đáo với khách hàng. Họ có tay nghề cao và chuyên nghiệp. Mình rất hài lòng."</p>
+                    <p class="name">Thắm Trần</p>
+                    <span class="position">Việt Nam</span>
+                </div>
+                </div>
+            </div>
+
+            <div class="item col-6 col-md-3">
+                <div class="testimony-wrap p-4 pb-5">
+                <div class="user-img mb-5" style="background-image: url(images/huan.jpg)">
+                    <span class="quote d-flex align-items-center justify-content-center">
+                    <i class="icon-quote-left"></i>
+                    </span>
+                </div>
+                <div class="text">
+                    <p class="mb-5 pl-4 line" style="min-height: 135px;">"Phương pháp làm trắng thảo dược là sự lựa chọn thông minh vì nó rất phù hợp với làn da người Lào mình. Khi mình tắm trắng thì da không hề bị khô và bong tróc, da trắng sáng tự nhiên. Đó là điều mình thích nhất!"</p>
+                    <p class="name">Thu Phương</p>
+                    <span class="position">Lào</span>
+                </div>
+                </div>
+            </div>
+
+            <div class="item col-6 col-md-3">
+                <div class="testimony-wrap p-4 pb-5">
+                <div class="user-img mb-5" style="background-image: url(images/hang.jpg)">
+                    <span class="quote d-flex align-items-center justify-content-center">
+                    <i class="icon-quote-left"></i>
+                    </span>
+                </div>
+                <div class="text">
+                    <p class="mb-5 pl-4 line" style="min-height: 135px;">"Những dịch vụ chăm sóc Da và Dáng ở Lina Spa là những dịch vụ khiến tôi hài lòng nhất, hiệu quả rất nhanh và lâu dài. Tôi đã tìm được một địa chỉ đáng tin cậy và đây sẽ là nơi tôi sẽ ghé đến thường xuyên sau những áp lực công việc và cuộc sống."</p>
+                    <p class="name">Nam Nguyễn</p>
+                    <span class="position">Cam-pu-chia</span>
+                </div>
+                </div>
+            </div>
+
+            <div class="item col-6 col-md-3">
+                <div class="testimony-wrap p-4 pb-5">
+                <div class="user-img mb-5" style="background-image: url(images/hieu.jpg)">
+                    <span class="quote d-flex align-items-center justify-content-center">
+                    <i class="icon-quote-left"></i>
+                    </span>
+                </div>
+                <div class="text">
+                    <p class="mb-5 pl-4 line" style="min-height: 135px;">"Đưa tay ra nào, mãi bên nhau bạn nhớ! <3"</p>
+                    <p class="name">Linh Hy</p>
+                    <span class="position">Thái Lan</span>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
